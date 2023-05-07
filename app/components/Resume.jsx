9cfb03944bfc4ae036eafa00659e0953bc528fac
Jeyran <jeylooj@gmail.com>
2023-05-07 17:28:58 +0200

    change background sections and hide overflow

--- app/components/Resume.jsx
+++ app/components/Resume.jsx
@@ -14,9 +14,9 @@ const Resume = () => {
    />
    <section className='flex flex-col mx-0 my-auto overflow-hidden items-center justify-center w-96 text-center gap-4'>
    <Image src='../Assets/SVG/Yellow Rectangle 5.svg' width={25} height={25} alt='square' className='rotate-90 absolute -top-28 left-96 invisible lg:visible'/>
-    <h3 className='text-xl'>Thanks for scrolling down, to here!</h3>
+    <h3 className='text-xl overflow-hidden'>Thanks for scrolling down, to here!</h3>
     <p className='text-tertiary'>You can easily download my Resume by clicking the button below! Thanks for considering it!</p>
-    <a className='bg-primary text-white w-1/2 rounded p-3 text-lg cursor-pointer' href='/Assets/Shahab.Resume.pdf' download={'Resume'} >Download Resume</a>
+    <a className='bg-primary text-white w-1/2 rounded p-3 text-lg cursor-pointer overflow-hidden' href='/Assets/Shahab.Resume.pdf' download={'Resume'} >Download Resume</a>
     <Image src='../Assets/SVG/Green Circle.svg' width={68} height={68} alt='square' className='rotate-45 absolute -bottom-32 -left-28 invisible lg:visible'/>
    </section>
    </section>
